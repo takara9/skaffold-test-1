db39d13d519161f3d947a1c04a4292d686e9002b
Maho Takara <takara9@gmail.com>
2019-05-16 17:07:12 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+ 
+import (
+  "log"
+  "net/http"
+)
+ 
+func main() {
+  http.Handle("/",http.StripPrefix("/", http.FileServer(http.Dir("/html"))))
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
